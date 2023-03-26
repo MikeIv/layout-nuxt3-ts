1943904c2afc280d538c3ed7cc1e7f0fbb46d833
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-26 20:35:28 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt 3 + ts',
+      meta: [{ name: 'description', content: 'Шаблон проекта' }],
+      link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }],
+    },
+  },
+});
